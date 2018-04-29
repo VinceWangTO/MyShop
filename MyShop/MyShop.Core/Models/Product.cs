958d84baad4907b109a486698d0ded27e0012afa
VinceWangTO <heng.w214@gmail.com>
2018-04-28 21:02:19 -0400

    Add data storage

--- MyShop/MyShop.Core/Models/Product.cs
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -12,9 +12,11 @@ namespace MyShop.Core.Models
     public class Product
     {
         public string Id { get; set; }
+
         [StringLength(20)]
         [DisplayName("Product Name")]
         public string Name { get; set; }
+
         public string Description { get; set; }
         public double Price { get; set; }
         public string Category { get; set; }
